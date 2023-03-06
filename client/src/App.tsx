4192fe4b321f35a5df17eb4aa606ee73f85b4545
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-06 19:13:55 +1300

    mc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Limitless Youth
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Limitless Youth
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
