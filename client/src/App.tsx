a42af03f6c2fe84244a78707d032eaadbca95969
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-08 21:13:25 +1300

    Added hero banner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import "./fonts/Surfing Capital.ttf";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeScreen from "./Views/Home/HomeScreen";
 import AboutScreen from "./Views/About/AboutScreen";
 import ContactScreen from "./Views/Contact/ContactScreen";
@@ -12,15 +11,6 @@ import Navbar from "./Views/Navbar/Navbar";
 const App = (): JSX.Element => {
     return (
         <div>
-            {/*<BrowserRouter>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/"        element={<HomeScreen />}/>*/}
-            {/*        <Route path="/about"   element={<AboutScreen />}/>*/}
-            {/*        <Route path="/contact" element={<ContactScreen />}/>*/}
-            {/*        <Route path="/notices" element={<NoticesScreen />}/>*/}
-            {/*        <Route path="/series"  element={<SeriesScreen />}/>*/}
-            {/*    </Routes>*/}
-            {/*</BrowserRouter>*/}
             <Navbar />
             <HomeScreen />
         </div>
