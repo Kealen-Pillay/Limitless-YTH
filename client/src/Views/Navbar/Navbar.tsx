6c7a726ba122d2f8aa4627103f70267cea1ff2a5
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-14 10:37:04 +1300

    added dropdown to hamburger menu

--- client/src/Views/Navbar/Navbar.tsx
+++ client/src/Views/Navbar/Navbar.tsx
@@ -2,23 +2,30 @@ import react from "react";
 import "./Navbar.css"
 
 const Navbar = (): JSX.Element => {
+
+    window.addEventListener("click",function () {
+        let hamburgerMenu:any = document.querySelector(".mobile-navbar");
+        hamburgerMenu.onclick = function() {
+            let webNavigationBar: any = document.querySelector(".web-navbar");
+            webNavigationBar.classList.toggle("active");
+        }
+    });
+
     return (
         <>
             <header>
-                <nav className="navigation">
-                    <div className="web-navbar">
-                        <a className="active" href="#home">Home</a>
-                        <a href="#notices">Events</a>
-                        <a href="#series">Series</a>
-                        <a href="#about">About</a>
-                        <a href="#contact">Contact</a>
-                    </div>
-                    <div className="mobile-navbar">
-                        <button>
-                            button
-                        </button>
-                    </div>
+                <nav className="web-navbar">
+                    <a className="active" href="#home">Home</a>
+                    <a href="#notices">Events</a>
+                    <a href="#series">Series</a>
+                    <a href="#about">About</a>
+                    <a href="#contact">Contact</a>
                 </nav>
+                <div className="mobile-navbar">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
             </header>
         </>
     );
