ae322f92709175a476cc8d95e6df4414834c752d
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-17 11:13:26 +1300

    mc

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,13 @@ import mongoose, {ConnectOptions} from "mongoose";
 import cors from "cors";
 import router from "./routes";
 
-const app: Express = express();
+const index: Express = express();
 const PORT: string | number = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(router);
+index.use(cors());
+index.use(router);
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.juadezf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const uri: string = `mongodb+srv://production:8qUHLItFypCQGtF1@cluster0.juadezf.mongodb.net/Limitless?retryWrites=true&w=majority`;
 const options = {
     useNewURLParser:    true,
     useUnifiedTopology: true,
@@ -17,7 +17,7 @@ const options = {
 
 mongoose.connect(uri, options)
     .then(() => {
-        app.listen(PORT, () => {
+        index.listen(PORT, () => {
             console.log(`Server running on PORT ${PORT}`);
         })
     })
