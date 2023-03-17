7a79ca0a7057c36b08b86cba50fe9d9806941a67
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-17 15:59:08 +1300

    added vercel.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,11 @@ import mongoose, {ConnectOptions} from "mongoose";
 import cors from "cors";
 import router from "./routes";
 
-const index: Express = express();
+const app: Express = express();
 const PORT: string | number = process.env.PORT || 5000;
 
-index.use(cors());
-index.use(router);
+app.use(cors());
+app.use(router);
 
 const uri: string = `mongodb+srv://production:8qUHLItFypCQGtF1@cluster0.juadezf.mongodb.net/Limitless?retryWrites=true&w=majority`;
 const options = {
@@ -17,7 +17,7 @@ const options = {
 
 mongoose.connect(uri, options)
     .then(() => {
-        index.listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log(`Server running on PORT ${PORT}`);
         })
     })
