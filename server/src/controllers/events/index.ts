ef0944752a3f1c1d30024fd08ded5bef1f012046
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-30 20:19:27 +1300

    commented out code

--- server/src/controllers/events/index.ts
+++ server/src/controllers/events/index.ts
@@ -7,10 +7,10 @@ const getEvents = async (req: Request, res: Response): Promise<void> => {
         const elapsedEvents: IEvent[] = [];
         const orderedEvents: IEvent[] = [];
         const events: IEvent[] = await Event.find();
-        events && events.map((event: IEvent) => {
-            event.elapsed ? elapsedEvents.push(event) : orderedEvents.push(event);
-        })
-        events.sort((a: IEvent, b: IEvent) => a.date.getTime() - b.date.getTime());
+        // events && events.map((event: IEvent) => {
+        //     event.elapsed ? elapsedEvents.push(event) : orderedEvents.push(event);
+        // })
+        // events.sort((a: IEvent, b: IEvent) => a.date.getTime() - b.date.getTime());
         res.status(200).json({ events });
     }
     catch (error) {
