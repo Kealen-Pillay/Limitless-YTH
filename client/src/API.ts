42607bd93f6b03c2625cf85820ab89cd714c41ea
Kealen-Pillay <kealenpillay@gmail.com>
2023-03-30 20:36:34 +1300

    changed baseURL

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from "axios";
 import {ApiAboutDataType, ApiEventDataType} from "./type";
 
-const baseUrl: string = "https://limitless-yth-git-development-kealen-pillay.vercel.app";
+const baseUrl: string = "https://limitless-yth.vercel.app/";
 // const baseUrl: string = "http://localhost:5000";
 export const getEvents = async (): Promise<AxiosResponse<ApiEventDataType>> => {
     try {
